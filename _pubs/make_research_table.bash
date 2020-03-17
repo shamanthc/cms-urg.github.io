23baed40e657c7736c44b4b33ad293c5eb2537b1
shamanthc <55105289+shamanthc@users.noreply.github.com>
2020-03-16 23:24:07 -0400

    Update make_research_table.bash

--- _pubs/make_research_table.bash
+++ _pubs/make_research_table.bash
@@ -3,4 +3,4 @@
 rm -f *.dat
 php combined.php | tail -n +4 > combined_table.html
 cat pub_header.txt combined_table.html > ../publications.html
-sed -i.bak -e '9,27d' ../publications.html 
+sed -i.bak -e '9,28d' ../publications.html 
